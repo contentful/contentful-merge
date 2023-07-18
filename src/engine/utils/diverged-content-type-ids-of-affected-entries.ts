49d8c00f1edbe3a3621d216524fe429b4a9cb0e9
Meike Pollakowski <meike.pollakowski@gmail.com>
2023-07-18 10:01:32 +0200

    feat: check for affected content types only with actual changes, don't consider removed

--- src/engine/utils/diverged-content-type-ids-of-affected-entries.ts
+++ src/engine/utils/diverged-content-type-ids-of-affected-entries.ts
@@ -7,15 +7,8 @@ export function divergedContentTypeIdsOfAffectedEntries(
   affectedEntities: Record<EntityType, AffectedEntityData>,
   entryComparables: Comparable[]
 ) {
-  // TODO this should ideally be actually changed content types, not maybeChanged
-  const divergedContentTypeIds = affectedEntitiesIds(affectedEntities.contentTypes, [
-    'added',
-    'removed',
-    'maybeChanged',
-  ])
-
-  // TODO this should ideally be actually changed entries, not maybeChanged
-  const affectedEntryIds = affectedEntitiesIds(affectedEntities.entries, ['added', 'maybeChanged'])
+  const divergedContentTypeIds = affectedEntitiesIds(affectedEntities.contentTypes, ['added', 'changed'])
+  const affectedEntryIds = affectedEntitiesIds(affectedEntities.entries, ['added', 'changed'])
 
   const contentTypeIdsOfAffectedEntries = [
     ...new Set<string>(
