b4bcd48c60f03c99a2d737c8a6c269c8f8969508
Janko Marklein <janko.marklein@contentful.com>
2023-07-19 10:12:24 +0200

    fix: remove finally and flush segment earlier for better UX

--- src/commands/create/index.ts
+++ src/commands/create/index.ts
@@ -195,26 +195,21 @@ export default class Create extends Command {
     // Any additional error handling or related user warnings should
     // go here if possible.
 
-    // TODO Move other errors to here as well, e.g. contentModelDiverged
-
     Sentry.captureException(error)
 
+    // TODO analyticsCloseAndFlush triggers an ExperimentalWarning as it uses the Fetch API and the task list to be
+    // rendered twice. Ideally this should be fixed or suppressed for better UX.
+    await Promise.allSettled([Sentry.close(2000), analyticsCloseAndFlush(2000)])
+
+    // TODO Move other errors to here as well, e.g. contentModelDiverged
     if (error instanceof AxiosError && error.code === 'ERR_BAD_REQUEST') {
       this.log(renderErrorOutput(new AccessDeniedError()))
-      this.log('about to exit')
-      this.exit()
+      this.exit(1)
     } else if (error instanceof Error) {
       this.log(renderErrorOutput(error))
-      this.exit()
+      this.exit(1)
     } else {
       throw error
     }
-  }
-
-  protected async finally(error: Error | undefined): Promise<any> {
-    // TODO analyticsCloseAndFlush triggers an ExperimentalWarning as it uses the Fetch API
-    // Ideally this should be fixed or suppressed for better UX.
-    await Promise.allSettled([Sentry.close(2000), analyticsCloseAndFlush(2000)])
-    return super.finally(error)
   }
 }
