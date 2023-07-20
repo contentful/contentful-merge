4b68ecca6a001be93f706a1360d7fdc45b6bf74c
Meike Pollakowski <meike.pollakowski@gmail.com>
2023-07-20 11:30:36 +0200

    feat: adjust integration test setup

--- test/integration/commands/create/bootstrap.ts
+++ test/integration/commands/create/bootstrap.ts
@@ -7,13 +7,18 @@ const randomId = Math.floor(Math.random() * 10000).toString() // used to prevent
 
 export type TestContext = {
   sourceEnvironment: Environment
+  targetEnvironment: Environment
   cdaToken: string
   spaceId: string
   teardown: () => Promise<void>
 }
 
 const waitForKeyReady = async (apiKey: ApiKey): Promise<void> => {
-  const client = createClient({ accessToken: apiKey.accessToken, space: apiKey.sys.space!.sys.id })
+  const client = createClient({
+    accessToken: apiKey.accessToken,
+    space: apiKey.sys.space!.sys.id,
+    environment: apiKey.environments[0].sys.id,
+  })
 
   const retries = 10
   for (let i = 0; i < retries; i++)
@@ -21,7 +26,7 @@ const waitForKeyReady = async (apiKey: ApiKey): Promise<void> => {
       await client.getEntries()
       return
     } catch {
-      const delayTime = 1000 * 2 ** i
+      const delayTime = Math.floor(1000 * 1.5 ** i)
       console.log(`CDA key not ready, retrying in ${delayTime}ms`)
       await new Promise((r) => setTimeout(r, delayTime))
     }
@@ -50,18 +55,20 @@ export const createCdaToken = async (
   return apiKey
 }
 
-const teardown = async (apiKey: ApiKey, environment: Environment): Promise<void> => {
-  await Promise.allSettled([apiKey.delete(), environment.delete()])
+const teardown = async ({ apiKey, environments }: { apiKey: ApiKey; environments: Environment[] }): Promise<void> => {
+  await Promise.allSettled([apiKey.delete(), ...environments.map((env) => env.delete())])
 }
 
-export const createEnvironment = async (testSpace: Space, targetEnvironmentId: string): Promise<TestContext> => {
-  const sourceEnvironment = await testUtils.createTestEnvironment(testSpace, randomId)
-  const apiKey = await createCdaToken(testSpace, [targetEnvironmentId, sourceEnvironment.sys.id])
+export const createEnvironments = async (testSpace: Space): Promise<TestContext> => {
+  const sourceEnvironment = await testUtils.createTestEnvironment(testSpace, randomId + 'source')
+  const targetEnvironment = await testUtils.createTestEnvironment(testSpace, randomId + 'target')
+  const apiKey = await createCdaToken(testSpace, [targetEnvironment.sys.id, sourceEnvironment.sys.id])
 
   return {
     sourceEnvironment,
+    targetEnvironment,
     cdaToken: apiKey.accessToken,
     spaceId: testSpace.sys.id,
-    teardown: () => teardown(apiKey, sourceEnvironment),
+    teardown: () => teardown({ apiKey, environments: [sourceEnvironment, targetEnvironment] }),
   }
 }
