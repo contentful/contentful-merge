097f7b2b99333ec8a8eaf2810e270fce69162494
Maya Karabula-Stysiak <maya.karabula@contentful.com>
2023-08-14 17:15:07 +0200

    feat: add more tests

---
+++ test/unit/engine/apply-changeset/tasks/create-change-entities-task.test.ts
@@ -0,0 +1,54 @@
+import { initializeTask } from '../../../test-utils'
+import { expect } from 'chai'
+import { createChangeEntitiesTask } from '../../../../../src/engine/apply-changeset/tasks/create-change-entities-task'
+import { beforeEach } from 'mocha'
+import { UpdatedChangesetItem } from '../../../../../src/engine/types'
+import { createLinkObject } from '../../../../../src/engine/utils/create-link-object'
+import * as sinon from 'sinon'
+import { createApplyChangesetContext } from '../../../fixtures/apply-changeset-context-fixture'
+import { ApplyChangesetContext } from '../../../../../src/engine/apply-changeset/types'
+
+describe('createChangeEntitiesTask', () => {
+  let context: ApplyChangesetContext
+  beforeEach(() => {
+    context = createApplyChangesetContext()
+  })
+
+  it('updates an entry from changeset', async () => {
+    const updatedChangesetItem: UpdatedChangesetItem = {
+      ...createLinkObject('update-entry', 'update', 'Entry'),
+      patch: [],
+    }
+
+    const updatedEntry = {
+      metadata: { tags: [] },
+      sys: {
+        space: { sys: { type: 'Link', linkType: 'Space', id: '529ziq3ce86u' } },
+        id: 'update-entry',
+        type: 'Entry',
+        createdAt: '2023-05-17T10:36:22.538Z',
+        updatedAt: '2023-05-17T10:36:40.280Z',
+        environment: { sys: { id: 'master', type: 'Link', linkType: 'Environment' } },
+        revision: 1,
+        contentType: { sys: { type: 'Link', linkType: 'ContentType', id: 'lesson' } },
+      },
+      fields: {},
+    }
+
+    context.client.cma.entries.update = sinon.stub().resolves(updatedEntry)
+    context.client.cma.entries.get = sinon.stub().resolves(updatedEntry)
+
+    context.changeset.items.push(updatedChangesetItem)
+
+    const task = initializeTask(createChangeEntitiesTask(), context)
+    let error = null
+    try {
+      await task.run()
+    } catch (err) {
+      error = err
+    }
+
+    expect(error).to.be.null
+    expect(task.tasks[0].output).to.be.equal('✨ successfully updated update-entry')
+  })
+})
