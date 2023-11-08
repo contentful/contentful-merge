998c52d3ef160de84f4ebd6bec9e2c9057f74ebe
Marco Link <marco.link@contentful.com>
2023-11-08 09:34:37 +0100

    fix: log file path [PHX-2947] (#527)
    Fix the file name pattern for the log file to align with the Changeset
    file name pattern
    Fixes #505

--- src/engine/logger/write-log.ts
+++ src/engine/logger/write-log.ts
@@ -3,7 +3,7 @@ import path from 'node:path'
 import { MemoryLogger } from './memory-logger'
 
 export const writeLog = async (logger: MemoryLogger): Promise<string> => {
-  const logName = path.join(process.cwd(), `log-${logger.getType()}-${new Date().toISOString()}.log`)
+  const logName = path.join(process.cwd(), `log-${logger.getType()}-${Date.now()}.log`)
   await fs.writeFile(logName, logger.toString())
   return logName
 }
